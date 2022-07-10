80e21354efe103b6b3be27dfd5f870038f00c0db
Yarik <yarik.prodan@gmail.com>
2022-07-10 19:14:58 +0300

    Added other scenes.

--- app/src/main/java/com/internship/firstweekapp/di/ViewModelModule.kt
+++ app/src/main/java/com/internship/firstweekapp/di/ViewModelModule.kt
@@ -1,11 +1,9 @@
 package com.internship.firstweekapp.di
 
 import com.internship.firstweekapp.ui.main.MainViewModel
-import com.internship.firstweekapp.ui.splash.SplashViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
-    viewModel { SplashViewModel() }
     viewModel { MainViewModel() }
 }
